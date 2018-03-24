f1adfed9277c4d32d026b23e28ff7ff9df7edb56
Kento75 <kentonet75@gmail.com>
2018-03-24 12:17:53 +0900

    画像登録処理の追加

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 
 var path = require('path');
 var bodyparser = require('body-parser');
+var fileUpload = require('express-fileupload');
 var mongoose = require('mongoose');
 
 var Message = require('./schema/Message');
@@ -24,25 +25,47 @@ app.use(bodyparser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use("/image", express.static(path.join(__dirname, 'image')));
+
 
 app.get("/", function(req, res, next) {
-  return res.render('index', {title: 'Hello World'});
+  Message.find({}, function(err, msgs){
+    if(err) throw err;
+    return res.render('index', {messages: msgs});
+  });
 });
 
-app.get("/update", function(req, res, next){
+
+app.get("/update", function(req, res, next) {
   return res.render('update');
 });
 
-app.post("/update", function(req, res, next) {
-  var newMessage = new Message({
-    username: req.body.username,
-    message: req.body.message
-  });
 
-  newMessage.save((err)=>{
-    if(err) throw err;
-    return res.redirect("/");
-  });
+app.post("/update", fileUpload(), function(req, res, next) {
+  if(req.files && req.files.image) {
+    req.files.image.mv('./image/' + req.files.image.name, function(err) {
+      if(err) throw err;
+
+      var newMessage = new Message({
+        username: req.body.username,
+        message: req.body.message,
+        image_path: '/image/' + req.files.image.name
+      });
+      newMessage.save((err) => {
+        if(err) throw err;
+        return res.redirect("/");
+      });
+    });
+  } else {
+    var newMessage = new Message({
+      username: req.body.username,
+      message: req.body.message
+    });
+    newMessage.save((err) => {
+      if(err) throw err;
+      return res.redirect("/");
+    });
+  }
 });
 
 var server = http.createServer(app);
--- schema/Message.js
+++ schema/Message.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var Message = mongoose.Schema({
   username: String,
   message: String,
-  date: {type: Date, default: new Date()}
+  date: {type: Date, default: new Date()},
+  image_path: String
 });
 
 module.exports = mongoose.model('Message', Message);
