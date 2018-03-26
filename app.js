481806ee930ac879f3615f12db2af2cb2c4f1479
Kento75 <kentonet75@gmail.com>
2018-03-27 06:21:03 +0900

    twitter認証の追加

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ var User = require('./schema/User');
 
 var app = express();
 
+// twitter設定
+var twitterConfig = {
+//  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+//  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+//  callbackURL: process.env.TWITTER_CALLBACK_URL,
+};
+
 // MongoDB接続
 mongoose.connect('mongodb://localhost:27017/chatapp', function(err) {
   if(err) {
@@ -62,20 +69,31 @@ app.get("/", function(req, res, next) {
   });
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findOne({ _id: id }, function(err, user) {
-    console.log("id:" + id);
-    done(err, user);
-  });
-});
+passport.use(new TwitterStrategy(twitterConfig,
+  function(token, tokenSecret, profile, done){
+    User.findOne({ twitter_profile_id: profile.id }, function (err, user) {
+      if (err) {
+        return done(err);
+      }else if (!user) {
+        var _user = {
+          username: profile.displayName,
+          twitter_profile_id: profile.id,
+          avatar_path: profile.photos[0].value
+        };
+        var newUser = new User(_user);
+        newUser.save((err)=>{
+          if(err) throw err
+          return done(null, newUser);
+        });
+      }else{
+        return done(null, user);
+      }
+    });
+  }
+));
 
-// 登録画面遷移
-app.get("/update", function(req, res, next) {
-  return res.render('update');
-});
-
-app.get("/oauth/twitter", passport.authenicate('twitter'));
-app.get("/oauth/twitter/callback", passport.authenicate('twitter'),
+app.get('/oauth/twitter', passport.authenticate('twitter'));
+app.get('/oauth/twitter/callback', passport.authenticate('twitter'),
   function(req, res, next) {
     User.findOne({
       _id: req.session.passport.user
@@ -83,6 +101,21 @@ app.get("/oauth/twitter/callback", passport.authenicate('twitter'),
       if(err || !req.session) return res.redirect('/oauth/twitter')
     })
   })
+
+  passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findOne({_id: id}, function(err, user) {
+      done(err, user);
+    });
+  });
+
+// 登録画面遷移
+app.get("/update", function(req, res, next) {
+  return res.render('update');
+});
 
 // 登録処理・一覧画面遷移
 app.post("/update", fileUpload(), function(req, res, next) {
